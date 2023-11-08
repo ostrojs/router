4ec9f1060a9549ed0dafbe742cf9ecb1c3a236fb
Amar kumar singh <amar@ostrojs.com>
2023-11-08 22:31:42 +0530

    code improvment

--- layer.js
+++ layer.js
@@ -17,61 +17,59 @@ function Layer(stack, opts = {}) {
     this.$regexp = pathToRegexp(stack.url, this.$keys = [], opts);
     this.$params = this.$keys;
 
-    this.$regexp.fast_star = stack.url === '*'
-    this.$regexp.fast_slash = stack.url === '/' && opts.end === false
+    this.$regexp.fast_star = stack.url === '*';
+    this.$regexp.fast_slash = stack.url === '/' && opts.end === false;
 }
 
 Layer.prototype.handle_error = function handle_error(error, ctx, next) {
     var fn = this.handle;
 
     if (fn.length !== 3) {
-
         return next(error);
     }
 
     try {
-        fn(error, ctx, next)
+        return fn(error, ctx, next);
     } catch (err) {
-        next(err);
+        return next(err);
     }
 };
 
 Layer.prototype.handle_request = function handle(ctx, next) {
     var fn = this.handle;
-    ctx.request.params = this.$params
+    ctx.request.params = this.$params;
 
     if (fn.length > 2) {
-
         return next();
     }
 
     try {
-        fn(ctx, next);
+        return fn(ctx, next);
     } catch (err) {
-        next(err);
+        return next(err);
     }
 };
 
 Layer.prototype.match = function match(path) {
-    var match
+    var match;
 
     if (path != null) {
 
         if (this.$regexp.fast_slash) {
-            this.$params = {}
-            this.$path = ''
-            return true
+            this.$params = {};
+            this.$path = '';
+            return true;
         }
 
         if (this.$regexp.fast_star) {
             this.$params = {
                 '0': decode_param(path)
-            }
-            this.$path = path
-            return true
+            };
+            this.$path = path;
+            return true;
         }
 
-        match = this.$regexp.exec(path)
+        match = this.$regexp.exec(path);
     }
 
     if (!match) {
@@ -81,7 +79,7 @@ Layer.prototype.match = function match(path) {
     }
 
     this.$params = {};
-    this.$path = match[0]
+    this.$path = match[0];
 
     var keys = this.$keys;
     var params = this.$params;
@@ -89,7 +87,7 @@ Layer.prototype.match = function match(path) {
     for (var i = 1; i < match.length; i++) {
         var key = keys[i - 1];
         var prop = key.name;
-        var val = decode_param(match[i])
+        var val = decode_param(match[i]);
 
         if (val !== undefined || !(hasOwnProperty.call(params, prop))) {
             params[prop] = val;
